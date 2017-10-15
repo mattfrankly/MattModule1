86b77804c4eab5f77ffab039e73fb651053276c4
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-15 19:37:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
 import _ from 'lodash';
-import getUsefulContents from './dep';
+// import getUsefulContents from './dep';
+
+function getJSON(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () { 
+    callback(this.responseText) 
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
 
 class MattModule1 extends Component {
   render(){
@@ -10,7 +19,7 @@ class MattModule1 extends Component {
     } = this.props;
 
      this.setState({results: ''});
-     getUsefulContents('https://modules-demo.franklyinc.com/latest.js?affiliate=wnow', data => { this.setState({results: data}); });
+     getJSON('https://modules-demo.franklyinc.com/latest.js?affiliate=wnow', data => { this.setState({results: data}); });
 
     let {
       flux, config
