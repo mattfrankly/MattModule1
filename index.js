0bbc671e2c945f68e7e717ea4b28d79e073bb723
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-15 19:33:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 import _ from 'lodash';
-import dep from './dep';
+import getUsefulContents from './dep';
 
 class MattModule1 extends Component {
   render(){
@@ -9,13 +9,17 @@ class MattModule1 extends Component {
       h2 = ''
     } = this.props;
 
+     let results = '';
+     getUsefulContents('http://www.example.com', data => { results = data; });
+
     let {
       flux, config
     } = this.context;
 
     return (
       <div>
-        <h2>{h2}</h2>
+        <h2>{h2}:</h2>
+        <pre>{results}</pre>
       </div>
     );
   }
