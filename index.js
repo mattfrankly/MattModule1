dbe77fd9f4134942df1bb61759786ac66a212696
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-15 19:47:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 // import _ from 'lodash';
 
+function getJSON(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () { 
+    callback(this.responseText) 
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
 class MattModule1 extends Component {
 
   constructor(props) {
     super(props);
     this.state = { results: {title:"test"} };
+    getJSON("https://modules-demo.franklyinc.com/latest.js?affiliate=wnow&1", function(data) {this.setState({results: data})} );
   }
   
   render(){
