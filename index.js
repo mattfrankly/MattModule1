17384f5a9a0fb08767f3f8b21735aff3bcdc15a1
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-15 20:04:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,21 +15,23 @@ class MattModule1 extends Component {
   constructor(props) {
     super(props);
     this.state = { results: "" };
-    getJSON("http://modules-demo.franklyinc.com/v1/affiliate/wnow", (data) => this.setState( {results: data} ) );
+    if (this.props.url) {
+      getJSON(this.props.url, (data) => this.setState( {results: data} ) );
+    }
   }
   
   render(){
-    let {
-          h2 = ''
-        } = this.props;
+//     let {
+//           h2 = ''
+//         } = this.props;
 
-    let {
-      flux, config
-    } = this.context;
+//     let {
+//       flux, config
+//     } = this.context;
 
     return (
       <div>
-        <h2>{h2}:</h2>
+        <h2>{this.props.h2}:</h2>
         <code>{this.state.results}</code>
       </div>
     );
